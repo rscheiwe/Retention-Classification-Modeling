f7233ae974b0b02818183ae7a1c1c525bae78bbb
rscheiwe <rscheiwe@gmail.com>
2018-03-07 20:53:20 -0500

    Model Class added

--- retention_model.py
+++ retention_model.py
@@ -20,37 +20,27 @@ class EmployeeRetentionModel:
             
         return X_new, self.model.predict_proba(X_new)
     
-    # Add functions here
+   
     def clean_data(self, df):
-        # Drop duplicates
         df = df.drop_duplicates()
 
-        # Drop temporary workers
         df = df[df.department != 'temp']
 
-        # Missing filed_complaint values should be 0
         df['filed_complaint'] = df.filed_complaint.fillna(0)
 
-        # Missing recently_promoted values should be 0
         df['recently_promoted'] = df.recently_promoted.fillna(0)
 
-        # 'information_technology' should be 'IT'
         df.department.replace('information_technology', 'IT', inplace=True)
 
-        # Fill missing values in department with 'Missing'
         df['department'].fillna('Missing', inplace=True)
 
-        # Indicator variable for missing last_evaluation
         df['last_evaluation_missing'] = df.last_evaluation.isnull().astype(int)
 
-        # Fill missing values in last_evaluation with 0
         df.last_evaluation.fillna(0, inplace=True)
 
-        # Return cleaned dataframe
         return df
     
     def engineer_features(self, df):
-        # Create indicator features
         df['underperformer'] = ((df.last_evaluation < 0.6) & 
                                 (df.last_evaluation_missing == 0)).astype(int)
 
@@ -58,28 +48,21 @@ class EmployeeRetentionModel:
 
         df['overachiever'] = ((df.last_evaluation > 0.8) & (df.satisfaction > 0.7)).astype(int)
 
-        # Create new dataframe with dummy features
         df = pd.get_dummies(df, columns=['department', 'salary'])
 
-        # Return augmented DataFrame
         return df
 
 
 def main(data_location, output_location, model_location, clean=True, augment=True):
-    # Read dataset
     df = pd.read_csv(data_location)
 
-    # Initialize model
     retention_model = EmployeeRetentionModel(model_location)
 
-    # Make prediction
     df, pred = retention_model.predict_proba(df)
     pred = [p[1] for p in pred]
 
-    # Add prediction to dataset
     df['prediction'] = pred
 
-    # Save dataset after making predictions
     df.to_csv(output_location, index=None)
 
 if __name__ == '__main__':
